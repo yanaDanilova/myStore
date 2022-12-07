ac01c781690073fbac143356f226ad1ce813842e
yanad <yana.danilova1509@gmail.com>
2022-12-07 15:05:30 +0100

    carts

--- cart-service/src/main/java/de/danilova/myStore/cart/services/CartService.java
+++ cart-service/src/main/java/de/danilova/myStore/cart/services/CartService.java
@@ -1,16 +1,12 @@
 package de.danilova.myStore.cart.services;
 
 import de.danilova.myStore.api.ProductDto;
-import de.danilova.myStore.api.ResourceNotFoundException;
 import de.danilova.myStore.cart.integrations.ProductServiceIntegration;
 import de.danilova.myStore.cart.models.Cart;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Service
@@ -19,36 +15,28 @@ public class CartService {
 
 
     private final ProductServiceIntegration productServiceIntegration;
-    private Map<String,Cart> carts;
-    @Value("${cart-service.cart-prefix}")
-    private String cartPrefix;
+    private Cart cart;
 
     @PostConstruct
     public void init() {
-        carts = new HashMap<>();
+        cart = new Cart();
 
     }
 
     public Cart getCurrentCart(String uuid){
-        String targetUuid = cartPrefix+uuid;
-        if(!carts.containsKey(targetUuid)){
-            carts.put(targetUuid,new Cart());
-        }
-        return carts.get(targetUuid);
+        return cart;
     }
 
     public void addToCart(String uuid, Long productId){
        ProductDto productDto = productServiceIntegration.getProductById(productId);
-       getCurrentCart(uuid).addProduct(productDto);
-
+        cart.addProduct(productDto);
     }
     public void clear(String uuid){
-        getCurrentCart(uuid).clear();
-
+        cart.clear();
     }
 
-    public void removeFromCart(String uuid,Long productId){
-        getCurrentCart(uuid).removeProduct(productId);
+    public void removeFromCart(String uuid, Long productId){
+        cart.removeProduct(productId);
     }
 
 }
